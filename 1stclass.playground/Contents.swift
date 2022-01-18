d22a1c3229fdd381ac00b511c88e63e85b1562a1
shahid mohammed <89232803+shahid987@users.noreply.github.com>
2022-01-18 10:45:08 -0600

    Add files via upload

---
+++ 1stclass.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print(greeting)
+var programmingLanguage="swift"
+print("My favorite programming language\(programmingLanguage)")
+var age=23
+var name="shahid"
+var grade=89
+print("you are years old and in another (age) years,you will be \(age)")
+print("welcome to swift programming",terminator:"-")
+print("my name is \(name)! I have got \(grade) percentage",terminator:" ")
+print(" in Fall 2021")
+var mobilebrand="apple"
+mobilebrand="samsung"
+print(mobilebrand)
+let pi=3.14
+print(pi)
+var age1:Int = 23
+age1 = age1*2
+var awseomemessage = "hello"
+print(awseomemessage)
+print("awseomemessage")
+var course="ios"
+var course1="patterns and frameworks"
+print(course,course1)
+print(course, "-", course1)
+print(10,20,30)
+print(12.5,15.5)
+var httperroor=(errorcode:404 , errormessage:"page not found")
+
+
+
